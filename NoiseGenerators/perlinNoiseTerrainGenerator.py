ebcd24a13a8f56c1c88922df76c56cbb7b3efad8
Max Techniche <max.techniche@gmail.com>
2019-12-08 22:24:26 -0700

    Update perlinNoiseTerrainGenerator.py

--- NoiseGenerators/perlinNoiseTerrainGenerator.py
+++ NoiseGenerators/perlinNoiseTerrainGenerator.py
@@ -5,19 +5,24 @@ from pygame.locals import *
 window_width = 1000
 window_height = 600
 window_size = (window_width, window_height)
-columns = 100
+columns = 30
 rows = 25
-angle = 10
+angle = 5
 horizon = window_height/3
+temp = window_width//columns
 
 class Point():
     def __init__(self, x, y=None, distance=None, height=None, connections=None):
         self.x = x
         self.y = y if y else horizon
-        self.distance = distance if distance else 0.1
-        self.height = random.randint(-2, 2)
+        self.distance = distance if distance else 2
+        self.distance_x = self.distance
+        self.height = height
+        if not self.height:
+            self.height = random.randint(-50, 50)
         self.connections = []
         self.draw_y = self.y + self.distance + self.height
+        self.linear_control = abs(self.x - window_width/2) * self.distance
 
     def __repr__(self):
         return str(self.x) + ' ' + str(self.y) + ' ' + str(self.distance) + ' ' + str(self.height)
@@ -47,19 +52,23 @@ class Point():
         if right:
             self.connections.append(right)
         
-    def move(self):
-        if self.x < window_width/2:
-            self.x -= len(point_grid[0]) / 2     #abs(self.x-(window_width//2)) * (self.distance / (window_width/angle))
-        elif self.x > window_width//2:
-            self.x += abs(self.x-(window_width//2)) * (self.distance / (window_width/angle))
-        self.y += self.distance
-        self.distance += .1
+    def move(self, row):
+        # if self.x < window_width/2:
+            # self.x -= self.linear_control # abs(self.x-(window_width//2)) * (1 / (window_width/angle))
+        # elif self.x > window_width//2:
+            # self.x += self.linear_control # abs(self.x-(window_width//2)) * (self.distance / (window_width/angle))
+        self.y += self.distance # if row >= 5 else .01
+        self.distance += .01
+        # self.draw_y = self.y + (self.height * 1/(row if 0 < row <= 5 else 1))
+        
+        # self.draw_y = horizon if row <= 5 else 
         self.draw_y = self.y + self.height
 
 
-point_grid = [[Point(x) for x in range(0, window_width+1, window_width//columns)]]
+point_grid = [[Point(x) for x in range(0, window_width+columns//10, window_width//columns)]]
+t = 0
 s = 0
-run = True
+run = False
 while run:
     point_row = []
     for point in point_grid[s]:
@@ -92,18 +101,60 @@ while running:
     
     for point_row in point_grid:
         for point in point_row:
-            pygame.draw.circle(window, (255, 255, 255), (int(point.x), int(point.y + point.height)), 1)
+            # pygame.draw.circle(window, (255, 255, 255), (int(point.x), int(point.y + point.height)), 1)
             for connected_point in point.connections:
                 pygame.draw.line(window, (255, 255, 255), (int(point.x), int(point.draw_y)), (int(connected_point.x), int(connected_point.draw_y)))
+    
+    for point_row in point_grid:
+        for point in point_row:
+            point_list = [(point.x, point.draw_y)]
+            for connected_point in point.connections:
+                point_list.append((connected_point.x, connected_point.draw_y))
+            if len(point_list) < 3:
+                pass
+            else:
+                pass # pygame.draw.polygon(window, 255, point_list)
+    
+    
     pygame.display.update()
 
     for x in range(len(point_grid)):
         for y in range(len(point_grid[x])):
-            point_grid[x][y].move()
-    if point_grid[-1][0].y > window_height:
+            point_grid[x][y].move(x)
+    if point_grid[-1][0].y > window_height + 100:
         del point_grid[-1]
-    point_grid.insert(0, [Point(x) for x in range(0, window_width+1, window_width//columns)])
+    
+    if t == 5:
+        new_row = []
+        x = 0
+        for point in range(len(point_grid[0])):
+            heights = []
+            try:
+                heights.append(point_grid[0][point].height)
+            except:
+                pass
+            try:
+                heights.append(point_grid[0][point-1].height)
+            except:
+                pass
+            try:
+                heights.append(point_grid[1][point+1].height)
+            except:
+                pass
+            try:
+                heights.append(point_grid[0][point+1].height)
+            except:
+                pass
+            height = int(sum(heights)/len(heights))
+            new_row.append(Point(x, height=random.randint(height-5, height+5)))
+            x += temp
+        point_grid.insert(0, new_row)
+        t = 0
+    else:
+        t += 1
+        
+    #point_grid.insert(0, [Point(x) for x in range(0, window_width+1, window_width//columns)])
     print(len(point_grid))
 
-
+pygame.quit()
 
